f2ec728c8a6d3508d99117f30a37b70352d8c289
ChandrasekarSubramani <73222496+ChandrasekarSubramani@users.noreply.github.com>
2022-12-23 11:56:13 +0530

    About us commit

--- src/components/AllCourseGallery/AllCourseGallery.tsx
+++ src/components/AllCourseGallery/AllCourseGallery.tsx
@@ -115,11 +115,10 @@ const AllCourseGallery = () => {
 
                 <Pagination className="d-flex justify-content-center mt-3">
                   <Pagination.Prev />
-                  <Pagination.Item>{1}</Pagination.Item>
+                  <Pagination.Item active>{1}</Pagination.Item>
                   <Pagination.Item>{2}</Pagination.Item>
                   <Pagination.Item>{3}</Pagination.Item>
                   <Pagination.Item>{4}</Pagination.Item>
-                  <Pagination.Item>{5}</Pagination.Item>
                   <Pagination.Next />
                 </Pagination>
               </Row>
