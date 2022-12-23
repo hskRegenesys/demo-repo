fa2331b7ac38a65a674e6b246eb5efbf58072146
Vivek Gupta <Vivekg@regenesys.net>
2022-12-23 17:39:33 +0530

    config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  presets: ["next/babel"],
   swcMinify: false, // it should be false by default
   eslint: {
     ignoreDuringBuilds: true,
