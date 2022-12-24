d53f4bdcc4dce5fc85b47bbdc2ea31c101fe6a65
Vivek Gupta <Vivekg@regenesys.net>
2022-12-24 18:25:51 +0530

    Form integration

--- src/data/axisos.tsx
+++ src/data/axisos.tsx
@@ -1,9 +1,13 @@
 export const ApiServiceConstants = {
   baseURL: process.env.NEXT_PUBLIC_DR_API_BASE_URL,
 };
+export const LeadApiServiceConstants = {
+  baseURL: process.env.base_Url,
+};
 export const apiEndPoints = Object.freeze({
   contactApi: "/contacts",
   coursesApi: "/course/all",
   parentCourseApi: "/course/parent",
   coursePriceApi: "/course/price",
+  leadApi: "/leads",
 });
--- src/services/index.tsx
+++ src/services/index.tsx
@@ -1,8 +1,11 @@
-import { ApiServiceConstants } from "@/data/axisos";
+import { ApiServiceConstants, LeadApiServiceConstants } from "@/data/axisos";
 import axios from "axios";
 import CourseService from "./CourseService";
+import LeadService from "./LeadServices";
 
 const appAPIServer = axios.create(ApiServiceConstants);
+const leadApiServer = axios.create(LeadApiServiceConstants);
 const courseService = new CourseService(appAPIServer);
+const leadService = new LeadService(leadApiServer);
 
-export { courseService };
+export { courseService, leadService };
