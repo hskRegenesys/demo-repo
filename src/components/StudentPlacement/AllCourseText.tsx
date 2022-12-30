89830d455ddfffc5b6bc6155cf6c10954a5ff6ab
Ravi Dubey <ravindrawebdeveloper2018@gmail.com>
2022-12-30 17:51:25 +0530

    conflict resolved

--- src/components/StudentPlacement/AllCourseText.tsx
+++ src/components/StudentPlacement/AllCourseText.tsx
@@ -10,34 +10,31 @@ const { title, pagedesc, imagearrow, text1, text2, highlight } = allCourse;
 const AllCourseText = () => {
   return (
     <section className="all-course-section all-course-wave">
-      <div className="FluidSection">   
-            <Col md={12} lg={12}>
-              <div className="all-course-bg">             
-              </div>
-            </Col>            
-        </div>
+      <div className="FluidSection">
+        <Col md={12} lg={12}>
+          <div className="all-course-bg"></div>
+        </Col>
+      </div>
 
-        <div className="auto-container">
-
-         <div className="content-box text-left">
-        <div className="sign-arrow">
-                <Image src={imagearrow} alt="" />
-              </div>  
+      <div className="auto-container">
+        <div className="content-box text-left">
+          <div className="sign-arrow">
+            <Image src={imagearrow} alt="" />
+          </div>
           <h3>
             <TextSplit text={title} />
           </h3>
           <h6 className="desc">{pagedesc}</h6>
 
           <Link href="">
-                <a className="theme-btn btn-style-two">
-                  <i className="btn-curve"></i>
-                  <span className="btn-title">Request a Call</span>
-                </a>
-              </Link>
+            <a className="theme-btn btn-style-two">
+              <i className="btn-curve"></i>
+              <span className="btn-title">Request a Call</span>
+            </a>
+          </Link>
         </div>
-        </div>
-        
-       </section>
+      </div>
+    </section>
   );
 };
 
--- src/components/courses/coursesText.tsx
+++ src/components/courses/coursesText.tsx
@@ -1,39 +1,46 @@
+import React, { useState } from "react";
+import Modal from "react-bootstrap/Modal";
 import { digitalMarketingText, dataScienceCourse } from "@/data/course";
 import Link from "next/link";
-import React from "react";
 import { Col, Image } from "react-bootstrap";
 import TextSplit from "../Reuseable/TextSplit";
+import ModalPopup from "../Modal/ModalPopup";
 
 const CourseText = ({ page }: any) => {
+  const [show, setShow] = useState(false);
+  const handleShow = () => setShow(true);
   const { title, pagedesc, imagearrow } =
     page === "Data-Science" ? dataScienceCourse : digitalMarketingText;
   return (
-    <section className="all-course-section all-course-wave">
-      <div className="FluidSection">
-        <Col md={12} lg={12}>
-          <div className="all-course-bg"></div>
-        </Col>
-      </div>
+    <>
+      <section className="all-course-section all-course-wave">
+        <div className="FluidSection">
+          <Col md={12} lg={12}>
+            <div className="all-course-bg"></div>
+          </Col>
+        </div>
 
-      <div className="auto-container">
-        <div className="content-box text-left digital-marketing-text mt-5">
-          <div className="sign-arrow">
-            <Image src={imagearrow} alt="" />
-          </div>
-          <h3>
-            <TextSplit text={title} />
-          </h3>
-          <h6 className="desc">{pagedesc}</h6>
+        <div className="auto-container">
+          <div className="content-box text-left digital-marketing-text mt-5">
+            <div className="sign-arrow">
+              <Image src={imagearrow} alt="" />
+            </div>
+            <h3>
+              <TextSplit text={title} />
+            </h3>
+            <h6 className="desc">{pagedesc}</h6>
 
-          <Link href="">
-            <a className="theme-btn btn-style-two">
+            <a className="theme-btn btn-style-two" onClick={handleShow}>
               <i className="btn-curve"></i>
               <span className="btn-title">Request a Call</span>
             </a>
-          </Link>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+      <Modal show={show}>
+        <ModalPopup setShows={setShow} />
+      </Modal>
+    </>
   );
 };
 
