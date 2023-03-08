1aec419ef89329c99d19cf2b2de23578ab59097a
Ravi Dubey <ravindrawebdeveloper2018@gmail.com>
2023-03-08 18:09:28 +0530

    landing page redir url added

--- next.config.js
+++ next.config.js
@@ -78,6 +78,88 @@ const redirectURL = () => {
       destination: "/terms-and-conditions",
       permanent: true,
     },
+
+    // landing pages
+
+    {
+      source: "/study-dr-programmes",
+      destination: "https://lp.digitalregenesys.com/study-dr-programmes",
+      permanent: true,
+    },
+    {
+      source: "/study-dr-programmes-01",
+      destination: "https://lp.digitalregenesys.com/study-dr-programmes-01",
+      permanent: true,
+    },
+    {
+      source: "/black-friday-dr",
+      destination: "https://lp.digitalregenesys.com/black-friday-dr",
+      permanent: true,
+    },
+    {
+      source: "/in/dataScience",
+      destination: "https://lp.digitalregenesys.com/landing/in/dataScience",
+      permanent: true,
+    },
+    {
+      source: "/sa/dataScience",
+      destination: "https://lp.digitalregenesys.com/landing/sa/dataScience",
+      permanent: true,
+    },
+    {
+      source: "/study-data-science-sa",
+      destination: "https://lp.digitalregenesys.com/study-data-science-sa",
+      permanent: true,
+    },
+    {
+      source: "/study-data-science-ng",
+      destination: "https://lp.digitalregenesys.com/study-data-science-ng",
+      permanent: true,
+    },
+    {
+      source: "/study-data-science-ky",
+      destination: "https://lp.digitalregenesys.com/study-data-science-ky",
+      permanent: true,
+    },
+    {
+      source: "/landing/sa/digital-marketing",
+      destination:
+        "https://lp.digitalregenesys.com/landing/sa/digital-marketing",
+      permanent: true,
+    },
+    {
+      source: "/study-digital-marketing-ng",
+      destination: "https://lp.digitalregenesys.com/study-digital-marketing-ng",
+      permanent: true,
+    },
+    {
+      source: "/study-cyber-security-sa",
+      destination: "https://lp.digitalregenesys.com/study-cyber-security-sa",
+      permanent: true,
+    },
+    {
+      source: "/study-cyber-security-ng",
+      destination: "https://lp.digitalregenesys.com/study-cyber-security-ng",
+      permanent: true,
+    },
+    {
+      source: "/landing/sa/project-management",
+      destination:
+        "https://lp.digitalregenesys.com/landing/sa/project-management",
+      permanent: true,
+    },
+    {
+      source: "/study-project-management-ky",
+      destination:
+        "https://lp.digitalregenesys.com/study-project-management-ky",
+      permanent: true,
+    },
+    {
+      source: "/study-project-management-ng",
+      destination:
+        "https://lp.digitalregenesys.com/study-project-management-ng",
+      permanent: true,
+    },
   ];
   return path;
 };
