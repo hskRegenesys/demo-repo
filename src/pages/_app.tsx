05a73cc01e7298f7b1b60b26ad9a7891f3475153
Ravi Dubey <ravindrawebdeveloper2018@gmail.com>
2023-03-16 19:05:48 +0530

    page optimization

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,7 +9,7 @@ import "@/vendors/reey-font.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "node_modules/swiper/swiper-bundle.min.css";
 import "react-circular-progressbar/dist/styles.css";
-// import "react-modal-video/css/modal-video.min.css";
+
 import "tiny-slider/dist/tiny-slider.css";
 
 // extra css
