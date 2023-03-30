5f2fa59ce76606857a8be9b35fe11783610ca330
Ravi Dubey <ravindrawebdeveloper2018@gmail.com>
2023-03-30 20:26:55 +0530

    console remove

--- src/pages/api/geoLocation.js
+++ src/pages/api/geoLocation.js
@@ -2,7 +2,6 @@ import axios from "axios";
 export default function handleSubmit(req, res) {
   if (req.method === "GET") {
     axios.get("https://geolocation-db.com/json").then(function (data) {
-      console.log("data====", data);
       res.send(data.data);
     });
   }
