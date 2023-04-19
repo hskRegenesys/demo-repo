9f53fbaed2349f0d505b720ed0d5bf5252d7fb28
unknown <Vedantc@regenesys.net>
2023-04-19 14:24:45 +0530

    ticket no 2427 compleated

--- src/components/CategoryBlogBody/Blogs.tsx
+++ src/components/CategoryBlogBody/Blogs.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Button } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import { wpService } from "src/services";
 
 const blogsData: { [key: string]: Array<{ [key: string]: string }> } = {
   "Popular Post": [
@@ -73,31 +73,63 @@ const blogsData: { [key: string]: Array<{ [key: string]: string }> } = {
 };
 
 const Blogs = () => {
+  const [response, setResponse] = useState<Array<any>>([]);
+
+  const getAllPosts = async () => {
+    const response = await wpService.allCategories();
+    !!response ? setResponse(response) : setResponse([]);
+    console.log(response);
+  };
+
+  useEffect(() => {
+    getAllPosts();
+  }, []);
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(!open);
+  };
   return (
     <div>
-      <div className="col-12 d-flex">
+      <div className="col-12 d-flex" style={{ zIndex: 100 }}>
         <h6 style={{ marginLeft: 10 }}>All Blogs</h6>
-        <div className="ms-auto" style={{ width: 350 }}>
-          <button
-            style={{
-              backgroundColor: "#289665",
-              width: 150,
-              marginRight: 10,
-              color: "white",
-            }}
-          >
-            Topic Filter
-          </button>
-          <button
-            style={{
-              backgroundColor: "#289665",
-              width: 150,
-              marginRight: 10,
-              color: "white",
-            }}
-          >
-            Sort By
-          </button>
+        <div className="d-flex flex-row  ms-auto p-2" style={{ zIndex: 100 }}>
+          <div className="dropdown" style={{ zIndex: 100 }}>
+            <button
+              onClick={handleOpen}
+              style={{
+                backgroundColor: "#289665",
+                width: 150,
+                marginRight: 10,
+                color: "white",
+              }}
+            >
+              Topic Filter
+            </button>
+            {open ? (
+              <div style={{ borderStyle: "solid" }}>
+                <ul className="menu">
+                  {response.map((item) => (
+                    <li className="menu-item">
+                      <div>{item.name}</div>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            ) : null}
+          </div>
+          <div className="dropdown">
+            <button
+              style={{
+                backgroundColor: "#289665",
+                width: 150,
+                marginRight: 10,
+                color: "white",
+              }}
+            >
+              Sort By
+            </button>
+          </div>
         </div>
       </div>
       {Object.keys(blogsData)?.map((value) => (
