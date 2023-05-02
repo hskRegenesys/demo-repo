a02cfa29eb3687464fcecab1beed8a5706281a2e
Ravi Dubey <ravindrawebdeveloper2018@gmail.com>
2023-05-02 14:57:54 +0530

    syntax error solved

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -69,7 +69,6 @@ const MyApp = ({ Component, pageProps }: any) => {
           _linkedin_partner_id = "5438577";
           window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
           window._linkedin_data_partner_ids.push(_linkedin_partner_id);
-          </script><script type="text/javascript">
           (function(l) {
           if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
           window.lintrk.q=[]}
@@ -78,7 +77,8 @@ const MyApp = ({ Component, pageProps }: any) => {
           b.type = "text/javascript";b.async = true;
           b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
           s.parentNode.insertBefore(b, s);})(window.lintrk);
-              `,
+          
+          `,
         }}
       />
       {/* linked end */}
@@ -109,17 +109,16 @@ const MyApp = ({ Component, pageProps }: any) => {
           style={{ display: "none", visibility: "hidden" }}
         ></iframe>
       </noscript>
-
-      {/* Google Tag Script End */}
-
       <noscript>
-        <iframe
-          src="https://px.ads.linkedin.com/collect/?pid=5438577&fmt=gif"
+        <img
           height="0"
           width="0"
           style={{ display: "none", visibility: "hidden" }}
-        ></iframe>
+          src="https://px.ads.linkedin.com/collect/?pid=5438577&fmt=gif"
+        />
       </noscript>
+
+      {/* Google Tag Script End */}
     </ContextProvider>
   );
 };
