f3bdd5ec522ffe14659020960b00908b1c53c81a
Ravi Dubey <ravindrawebdeveloper2018@gmail.com>
2023-05-26 19:42:30 +0530

    blog banner home page updated

--- src/components/blogsBody/Carousel.tsx
+++ src/components/blogsBody/Carousel.tsx
@@ -15,30 +15,43 @@ function CarouselComponent({ carouselProps = [] }: ICarouselProps) {
 
   return (
     <Carousel activeIndex={index} onSelect={handleSelect}>
-      {carouselProps?.map(({ image, label, caption }, index) => (
-        <Carousel.Item key={index}>
-          <div
-            className="w-100 position-relative"
-            style={{ minHeight: "500px" }}
-          >
-            <Image
-              src={`/assets/images/background/${image}`}
-              alt={`${image}-index`}
-              layout="fill"
-              objectFit="contain"
-            />
-          </div>
+      {carouselProps?.map(
+        ({ desktopImage, mobileImage, label, caption }, index) => (
+          <Carousel.Item key={index}>
+            <div
+              className="w-100 position-relative d-none d-md-block "
+              style={{ minHeight: "500px" }}
+            >
+              <Image
+                src={`/assets/images/background/${desktopImage}`}
+                alt={`${desktopImage}-index`}
+                layout="fill"
+                objectFit="contain"
+              />
+            </div>
+            <div
+              className="w-100 position-relative d-block d-md-none"
+              style={{ minHeight: "500px" }}
+            >
+              <Image
+                src={`/assets/images/background/${mobileImage}`}
+                alt={`${mobileImage}-index`}
+                layout="fill"
+                objectFit="cover"
+              />
+            </div>
 
-          <Carousel.Caption className="d-flex align-item-center justify-content-center flex-column">
-            {label && (
-              <div>
-                <h3 className="text-light carousal-text">{label}</h3>
-              </div>
-            )}
-            {caption && <p className="text-light carousal-text">{caption}</p>}
-          </Carousel.Caption>
-        </Carousel.Item>
-      ))}
+            <Carousel.Caption className="d-flex align-item-center justify-content-center flex-column">
+              {label && (
+                <div>
+                  <h3 className="text-light carousal-text">{label}</h3>
+                </div>
+              )}
+              {caption && <p className="text-light carousal-text">{caption}</p>}
+            </Carousel.Caption>
+          </Carousel.Item>
+        )
+      )}
     </Carousel>
   );
 }
