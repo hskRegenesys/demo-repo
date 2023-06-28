c7ac63cc7f005a1aac32ab431723a12145d992dc
Ravi Dubey <ravindrawebdeveloper2018@gmail.com>
2023-06-28 16:01:05 +0530

    certificate contents added

--- src/components/CourseCertificate/CourseCertificate.js
+++ src/components/CourseCertificate/CourseCertificate.js
@@ -4,7 +4,16 @@ import { Modal } from "react-bootstrap";
 import ZoomInOutlinedIcon from "@mui/icons-material/ZoomInOutlined";
 import NavigateNextIcon from "@mui/icons-material/NavigateNext";
 const CourseCertificate = ({ courseDetails }) => {
-  // const { title, subTitle } = courseDetails?.courseCertificate;
+  const {
+    certificateTitle,
+    certificateSubTitle,
+    certificateSubSubTitle,
+    certificateListPara,
+    certificateListTitle,
+    certificateSkillList,
+    certificateRolesList,
+    CertificateImage,
+  } = courseDetails?.courseCertificate;
   const [showImage, setShowImage] = useState(false);
   const toggleModal = () => {
     setShowImage(!showImage);
@@ -13,57 +22,27 @@ const CourseCertificate = ({ courseDetails }) => {
     <div className="certificate-row">
       <div className="row">
         <div className="col-lg-6">
-          <h2>Masterful Certification in Digital Marketing Fundamentals</h2>
+          <h2>{certificateTitle}</h2>
           <div className="certificate-content">
-            <strong>
-              Unleash Your Digital Marketing Potential with Digital Regenesys’s
-              Digital Marketing course.
-            </strong>
-            <span>
-              With our Digital Marketing programme, you can gain the skills and
-              knowledge to stay ahead in the competition by working on projects,
-              simulations, and cases.
-            </span>
-            <span>With Digital Marketing Certificate, you will get:</span>
+            <strong>{certificateSubTitle}</strong>
+            <span>{certificateSubSubTitle}</span>
+            <span>{certificateListTitle}</span>
             <ul>
-              <li>
-                <NavigateNextIcon />
-                International Recognition
-              </li>
-              <li>
-                <NavigateNextIcon /> Industry-Relevant Curriculum
-              </li>
-              <li>
-                <NavigateNextIcon /> Hands-on Experience
-              </li>
-              <li>
-                <NavigateNextIcon /> Tools and Technologies
-              </li>
-              <li>
-                <NavigateNextIcon /> Data-Driven Decision Making
-              </li>
+              {certificateSkillList?.map((item) => (
+                <li>
+                  <NavigateNextIcon />
+                  {item}
+                </li>
+              ))}
             </ul>
-            <span>
-              Our Digital Marketing Certificate will open doors to various roles
-              for you, whether you are a fresher, graduate or experienced
-              professional.
-            </span>
+            <span>{certificateListPara}</span>
             <ul>
-              <li>
-                <NavigateNextIcon /> Digital Marketing Specialist
-              </li>
-              <li>
-                <NavigateNextIcon /> Social Media Manager
-              </li>
-              <li>
-                <NavigateNextIcon /> PPC Specialist
-              </li>
-              <li>
-                <NavigateNextIcon /> Email Marketing Specialist
-              </li>
-              <li>
-                <NavigateNextIcon /> E-commerce Specialist
-              </li>
+              {certificateRolesList?.map((item) => (
+                <li>
+                  <NavigateNextIcon />
+                  {item}
+                </li>
+              ))}
             </ul>
           </div>
         </div>
@@ -71,7 +50,7 @@ const CourseCertificate = ({ courseDetails }) => {
           <div className="certificate-sample">
             <div className="certificate-image">
               <Image
-                src="/assets/images/background/DR-Sample_certificate.jpg"
+                src={`/assets/images/background/${CertificateImage}`}
                 layout="intrinsic"
                 width="550"
                 height="394"
