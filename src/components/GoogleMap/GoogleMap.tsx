d77fe1cc0e129f7b0d881858d226bf2a39d01038
Ravi Dubey <ravindrawebdeveloper2018@gmail.com>
2023-07-17 11:54:12 +0530

    campus image updated and map content is updated

--- src/components/GoogleMap/GoogleMap.tsx
+++ src/components/GoogleMap/GoogleMap.tsx
@@ -7,17 +7,25 @@ const GoogleMap = () => {
   return (
     <div className="google-map">
       <div className="container">
-        <div className="row mt-4">
+        <div className="row align-items-center mt-4">
           <div className="col-lg-4">
-            <h3>Our Corporate Presence</h3>
-            <h4>Innovative. Innovative. Innovative.</h4>
+            <h3>CORPORATE PRESENCE</h3>
+            <h4>Innovative. Transformative. Trailblazing.</h4>
             <p>
-              Unlock your career potential with our expert career guidance
-              services, designed to help you navigate the ever-evolving job
-              market and achieve your professional goals. Unlock your career
-              potential with our expert career guidance services, designed to
-              help you navigate the ever-evolving job market and achieve your
-              professional goals.
+              Digital Regenesys is a leading ed-tech platform that offers a wide
+              array of upskilling courses designed to empower individuals and
+              organizations with the latest industry-relevant skills. With its
+              headquarters situated in Silicon Valley, the world's technological
+              hub, Digital Regenesys operates globally, including a strong
+              presence in South Africa, Nigeria, Kenya, India, and beyond. Our
+              mission is to bridge the skills gap and drive economic growth by
+              providing learners with access to high-quality, cutting-edge
+              education. Whether it's in the field of technology, business, or
+              creative disciplines, Digital Regenesys offers comprehensive
+              programs that cater to diverse learning needs. Through our global
+              presence, we strive to empower learners from different regions to
+              thrive in their respective professional landscapes, contribute to
+              innovation, and shape the future of work.
             </p>
           </div>
           <div className="col-lg-8">
