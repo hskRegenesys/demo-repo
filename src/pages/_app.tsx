ec0df30b34dd435f75545c26ef2125a39bb5a547
Ravi Dubey <ravindrawebdeveloper2018@gmail.com>
2023-08-12 13:15:04 +0530

    chatbot changes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -162,14 +162,14 @@ const MyApp = ({ Component, pageProps }: any) => {
         dangerouslySetInnerHTML={{
           __html: `
           var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-        (function(){
-        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-        s1.async=true;
-        s1.src='https://embed.tawk.to/64d62ea7cc26a871b02ea9bf/1h7iacgka';
-        s1.charset='UTF-8';
-        s1.setAttribute('crossorigin','*');
-        s0.parentNode.insertBefore(s1,s0);
-        })();
+          (function(){
+          var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+          s1.async=true;
+          s1.src='https://embed.tawk.to/64d7316f94cf5d49dc69f045/1h7k9i7u2';
+          s1.charset='UTF-8';
+          s1.setAttribute('crossorigin','*');
+          s0.parentNode.insertBefore(s1,s0);
+          })();
               `,
         }}
       />
