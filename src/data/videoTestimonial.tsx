b2ac54d2fe7710067b029bd9d1a2a2c854f23608
Ravi Dubey <ravindrawebdeveloper2018@gmail.com>
2023-10-05 15:35:12 +0530

    video testimonials updated video testimonial changes and testimonials changes

--- src/data/videoTestimonial.tsx
+++ src/data/videoTestimonial.tsx
@@ -1,6 +1,21 @@
 const videoTestimonialData = {
   videos: [
     {
+      url: "https://www.youtube.com/embed/GRHdCKAyPWA?si=T8SVs70NoowFSax5&rel=0",
+    },
+    {
+      url: "https://www.youtube.com/embed/PHZ5tSbHNLA?si=CbdjYoFf50n-QxfT&rel=0",
+    },
+    {
+      url: "https://www.youtube.com/embed/stdQQ27vPO0?si=S0K_DnkR-VMB-tRH&rel=0",
+    },
+    {
+      url: "https://www.youtube.com/embed/QU5uTR7QbfU?si=V0iH3I9kHB3F6_2X&rel=0",
+    },
+    {
+      url: "https://www.youtube.com/embed/lW4vEYs7u-o?si=dSVGVQtXCfI_mn7V&rel=0",
+    },
+    {
       url: "https://www.youtube.com/embed/ygZIi5tRC40?si=dvLoIQrpMXvgHkn4&rel=0",
     },
     {
