1819be7825be505eb51488c08a39e9e096d87d8a
Ravi Dubey <ravindrawebdeveloper2018@gmail.com>
2023-11-16 13:00:01 +0530

    blog gtm code added

--- src/pages/blog/category/[slug].tsx
+++ src/pages/blog/category/[slug].tsx
@@ -7,21 +7,44 @@ import SearchPopup from "@/components/SearchPopup/SearchPopup";
 import MainFooter from "@/components/MainFooter/MainFooter";
 import StickyBar from "@/components/StickyFooter/Sticky";
 import BlogsByCategories from "@/components/blogsBody/BlogsByCategories";
+import Script from "next/script";
 
 const PostsByCategory = () => {
   const router = useRouter();
   const { slug } = router.query;
 
   return (
-    <Layout pageTitle="Blogs">
-      <Style />
-      <HeaderOne variant="blog" />
-      <MobileMenu />
-      <SearchPopup />
-      {slug && <BlogsByCategories categorySlug={slug.toString()} />}
-      <MainFooter />
-      <StickyBar />
-    </Layout>
+    <>
+      <Script
+        strategy="beforeInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-MSZ536C5');
+              `,
+        }}
+      />
+      <Layout pageTitle="Blogs">
+        <Style />
+        <HeaderOne variant="blog" />
+        <MobileMenu />
+        <SearchPopup />
+        {slug && <BlogsByCategories categorySlug={slug.toString()} />}
+        <MainFooter />
+        <StickyBar />
+      </Layout>
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-MSZ536C5"
+          height="0"
+          width="0"
+          style={{ display: "none", visibility: "hidden" }}
+        ></iframe>
+      </noscript>
+    </>
   );
 };
 
--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -8,6 +8,7 @@ import MainFooter from "@/components/MainFooter/MainFooter";
 import StickyBar from "@/components/StickyFooter/Sticky";
 import BlogsBody from "@/components/blogsBody";
 import { wpService } from "src/services";
+import Script from "next/script";
 
 const Blogs = () => {
   const [searchData, setSearchData] = useState([]);
@@ -22,15 +23,38 @@ const Blogs = () => {
   };
 
   return (
-    <Layout pageTitle="blog">
-      <Style />
-      <HeaderOne variant="blog" />
-      <MobileMenu />
-      <SearchPopup />
-      <BlogsBody />
-      <MainFooter />
-      <StickyBar />
-    </Layout>
+    <>
+      <Script
+        strategy="beforeInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-MSZ536C5');
+              `,
+        }}
+      />
+      <Layout pageTitle="blog">
+        <Style />
+        <HeaderOne variant="blog" />
+        <MobileMenu />
+        <SearchPopup />
+        <BlogsBody />
+        <MainFooter />
+        <StickyBar />
+      </Layout>
+
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-MSZ536C5"
+          height="0"
+          width="0"
+          style={{ display: "none", visibility: "hidden" }}
+        ></iframe>
+      </noscript>
+    </>
   );
 };
 
