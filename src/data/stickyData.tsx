dee795a88e02fa645c496464d7c86e5e470ddf93
Ravi Dubey <ravindrawebdeveloper2018@gmail.com>
2023-11-24 14:38:03 +0530

    sticky header change

--- src/data/stickyData.tsx
+++ src/data/stickyData.tsx
@@ -3,51 +3,48 @@ interface IAllPageStickyDataType {
 }
 const AllPageStickyData: IAllPageStickyDataType = {
   "cyber-security":
-    "Black Friday Discount On All Courses!, limited to the first 100 Seats",
-  home: "Black Friday Discount On All Courses!, limited to the first 100 Seats",
-
+    "Black Friday Discount On All Courses! Last 3 Days to Avail",
+  home: "Black Friday Discount On All Courses! Last 3 Days to Avail",
 
   "digital-marketing":
-    "Black Friday Discount On All Courses!, limited to the first 100 Seats",
+    "Black Friday Discount On All Courses! Last 3 Days to Avail",
   "basic-digital-marketing":
-    "Black Friday Discount On All Courses!, limited to the first 100 Seats",
+    "Black Friday Discount On All Courses! Last 3 Days to Avail",
   "advanced-digital-marketing":
-    "Black Friday Discount On All Courses!, limited to the first 100 Seats",
+    "Black Friday Discount On All Courses! Last 3 Days to Avail",
   "applied-digital-marketing":
-    "Black Friday Discount On All Courses!, limited to the first 100 Seats",
+    "Black Friday Discount On All Courses! Last 3 Days to Avail",
   "project-management":
-    "Black Friday Discount On All Courses!, limited to the first 100 Seats",
-  "all-courses":
-    "Black Friday Discount On All Courses!, limited to the first 100 Seats",
-  "data-science":
-    "Black Friday Discount On All Courses!, limited to the first 100 Seats",
+    "Black Friday Discount On All Courses! Last 3 Days to Avail",
+  "all-courses": "Black Friday Discount On All Courses! Last 3 Days to Avail",
+  "data-science": "Black Friday Discount On All Courses! Last 3 Days to Avail",
   "basic-data-science":
-    "Black Friday Discount On All Courses!, limited to the first 100 Seats",
+    "Black Friday Discount On All Courses! Last 3 Days to Avail",
   "advanced-data-science":
-    "Black Friday Discount On All Courses!, limited to the first 100 Seats",
+    "Black Friday Discount On All Courses! Last 3 Days to Avail",
   "applied-data-science":
-    "Black Friday Discount On All Courses!, limited to the first 100 Seats",
+    "Black Friday Discount On All Courses! Last 3 Days to Avail",
   "cyber-defense-toolbox-course":
-    "Black Friday Discount On All Courses!, limited to the first 100 Seats",
+    "Black Friday Discount On All Courses! Last 3 Days to Avail",
 
   "digital-marketing-fundamentals":
-    "Black Friday Discount On All Courses!, limited to the first 100 Seats",
+    "Black Friday Discount On All Courses! Last 3 Days to Avail",
   "advance-digital-marketing-course":
-    "Black Friday Discount On All Courses!, limited to the first 100 Seats",
+    "Black Friday Discount On All Courses! Last 3 Days to Avail",
   "cyber-security-fundamentals-course":
-    "Black Friday Discount On All Courses!, limited to the first 100 Seats",
+    "Black Friday Discount On All Courses! Last 3 Days to Avail",
   "design-thinking":
-    "Black Friday Discount On All Courses!, limited to the first 100 Seats",
+    "Black Friday Discount On All Courses! Last 3 Days to Avail",
   "cyber-security-course":
-    "Black Friday Discount On All Courses!, limited to the first 100 Seats",
+    "Black Friday Discount On All Courses! Last 3 Days to Avail",
   "artificial-intelligence":
-    "Black Friday Discount On All Courses!, limited to the first 100 Seats",
+    "Black Friday Discount On All Courses! Last 3 Days to Avail",
   "ai-introductory":
-    " Black Friday Discount On All Courses!, limited to the first 100 Seats",
+    " Black Friday Discount On All Courses! Last 3 Days to Avail",
   "ai-intermediary":
-    "Black Friday Discount On All Courses!, limited to the first 100 Seats",
+    "Black Friday Discount On All Courses! Last 3 Days to Avail",
   "ai-advanced-applied":
-    "Black Friday Discount On All Courses!, limited to the first 100 Seats",
+    "Black Friday Discount On All Courses! Last 3 Days to Avail",
 };
 
 export default AllPageStickyData;
