517e282edf12f4538aaacb5a4b0f0c9015798dd7
unknown <gunasekarr@regenesys.net>
2023-11-29 19:36:12 +0530

    DR_website-banner-change-nov-29

--- src/data/stickyData.tsx
+++ src/data/stickyData.tsx
@@ -2,49 +2,46 @@ interface IAllPageStickyDataType {
   [key: string]: string;
 }
 const AllPageStickyData: IAllPageStickyDataType = {
-  "cyber-security":
-    " Black Friday Discount On All Courses! Last 2 Days to Avail",
-  home: " Black Friday Discount On All Courses! Last 2 Days to Avail",
+  "cyber-security": " Black Friday Discount On All Courses! Last Day to Avail",
+  home: " Black Friday Discount On All Courses! Last Day to Avail",
 
   "digital-marketing":
-    " Black Friday Discount On All Courses! Last 2 Days to Avail",
+    " Black Friday Discount On All Courses! Last Day to Avail",
   "basic-digital-marketing":
-    " Black Friday Discount On All Courses! Last 2 Days to Avail",
+    " Black Friday Discount On All Courses! Last Day to Avail",
   "advanced-digital-marketing":
-    " Black Friday Discount On All Courses! Last 2 Days to Avail",
+    " Black Friday Discount On All Courses! Last Day to Avail",
   "applied-digital-marketing":
-    " Black Friday Discount On All Courses! Last 2 Days to Avail",
+    " Black Friday Discount On All Courses! Last Day to Avail",
   "project-management":
-    " Black Friday Discount On All Courses! Last 2 Days to Avail",
-  "all-courses": " Black Friday Discount On All Courses! Last 2 Days to Avail",
-  "data-science": " Black Friday Discount On All Courses! Last 2 Days to Avail",
+    " Black Friday Discount On All Courses! Last Day to Avail",
+  "all-courses": " Black Friday Discount On All Courses! Last Day to Avail",
+  "data-science": " Black Friday Discount On All Courses! Last Day to Avail",
   "basic-data-science":
-    " Black Friday Discount On All Courses! Last 2 Days to Avail",
+    " Black Friday Discount On All Courses! Last Day to Avail",
   "advanced-data-science":
-    " Black Friday Discount On All Courses! Last 2 Days to Avail",
+    " Black Friday Discount On All Courses! Last Day to Avail",
   "applied-data-science":
-    " Black Friday Discount On All Courses! Last 2 Days to Avail",
+    " Black Friday Discount On All Courses! Last Day to Avail",
   "cyber-defense-toolbox-course":
-    " Black Friday Discount On All Courses! Last 2 Days to Avail",
+    " Black Friday Discount On All Courses! Last Day to Avail",
 
   "digital-marketing-fundamentals":
-    " Black Friday Discount On All Courses! Last 2 Days to Avail",
+    " Black Friday Discount On All Courses! Last Day to Avail",
   "advance-digital-marketing-course":
-    " Black Friday Discount On All Courses! Last 2 Days to Avail",
+    " Black Friday Discount On All Courses! Last Day to Avail",
   "cyber-security-fundamentals-course":
-    " Black Friday Discount On All Courses! Last 2 Days to Avail",
-  "design-thinking":
-    " Black Friday Discount On All Courses! Last 2 Days to Avail",
+    " Black Friday Discount On All Courses! Last Day to Avail",
+  "design-thinking": " Black Friday Discount On All Courses! Last Day to Avail",
   "cyber-security-course":
-    " Black Friday Discount On All Courses! Last 2 Days to Avail",
+    " Black Friday Discount On All Courses! Last Day to Avail",
   "artificial-intelligence":
-    " Black Friday Discount On All Courses! Last 2 Days to Avail",
+    " Black Friday Discount On All Courses! Last Day to Avail",
   "ai-introductory":
-    "  Black Friday Discount On All Courses! Last 2 Days to Avail",
-  "ai-intermediary":
-    " Black Friday Discount On All Courses! Last 2 Days to Avail",
+    "  Black Friday Discount On All Courses! Last Day to Avail",
+  "ai-intermediary": " Black Friday Discount On All Courses! Last Day to Avail",
   "ai-advanced-applied":
-    " Black Friday Discount On All Courses! Last 2 Days to Avail",
+    " Black Friday Discount On All Courses! Last Day to Avail",
 };
 
 export default AllPageStickyData;
