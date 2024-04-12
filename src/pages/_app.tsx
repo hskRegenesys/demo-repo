88edbe0b6d00f6811d03a2bca5748e986fa33e34
Ravi Dubey <ravindrawebdeveloper2018@gmail.com>
2024-04-12 15:27:10 +0530

    chat active script

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -201,7 +201,7 @@ const MyApp = ({ Component, pageProps }: any) => {
 
       {/*End Google Tag Manager (noscript) */}
 
-      {/* <Script
+      <Script
         type="text/javascript"
         dangerouslySetInnerHTML={{
           __html: `
@@ -216,7 +216,7 @@ const MyApp = ({ Component, pageProps }: any) => {
           })();
               `,
         }}
-      /> */}
+      />
 
       <Script
         strategy="lazyOnload"
@@ -234,71 +234,59 @@ const MyApp = ({ Component, pageProps }: any) => {
       window.Tawk_API = window.Tawk_API || {};
       window.Tawk_API.onPrechatSubmit = function(data){
         const salesForceUrl = '${vineCrmTawk}';
-        console.log("salesForceUrl",salesForceUrl)
+        console.log("salesForceUrl", salesForceUrl);
+
         const salesForceData = {
-          // recordTypeId:"0127Q000000NDbcQAG",
-          // interestedTopic:"",
-          // highestQualification:"",
-          // utm_parameters:"",
-          // Mode_of_Study:"",
-          // Verified_Mobile_No:"",
-          // utm_source: "DR website chat ",
-          // utm_medium: "DR Website",
-          // utm_campaign: "DR Website",
-          // Source_Campaign:"DR Website",
-          // Lead_Source:"DR website chat"
-
-
           domain: "crm",
           type: "add_lead_to_crm",
           name: "",
           email: "",
           mobile: "",
           city: "",
-          utm_source: "DR website chat ",
+          utm_source: "DR website chat",
           utm_medium: "DR Website",
           utm_campaign: "DR Website",
-          Source_Campaign:"DR Website",
-          Lead_Source:"DR website chat"
+          Source_Campaign: "DR Website",
+          Lead_Source: "DR website chat",
           utm_term: crmData.utm_term,
           utm_content: crmData.utm_content,
           country: crmCountryCode,
-          interest: "",
-          
-          
-        };   
-        console.log("salesForceData", salesForceData)
+          interest: ""
+        };
+
         data.forEach(item => {
-          console.log("item",item)
+          console.log("item", item);
           const labelMapping = {
-              "Name": "name",
-              "Email": "email",
-              "Mobile Number": "mobile",
-              "Course you are looking for": "interest"
+            "Name": "name",
+            "Email": "email",
+            "Mobile Number": "mobile",
+            "Course you are looking for": "interest"
           };
-          const propertyName = labelMapping[item.label] || item.label; 
+          const propertyName = labelMapping[item.label] || item.label;
           salesForceData[propertyName] = item.answer;
-      });
-      console.log("vineCrmData",vineCrmData)
-      fetch(salesForceUrl, {
-        method: 'POST',
-        headers: {
+        });
+
+        console.log("salesForceData", salesForceData);
+
+        fetch(salesForceUrl, {
+          method: 'POST',
+          headers: {
             'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(salesForceData),
-    })
-    .then(response => {
-        if (!response.ok) {
+          },
+          body: JSON.stringify(salesForceData),
+        })
+        .then(response => {
+          if (!response.ok) {
             throw new Error('Network response was not ok');
-        }
-        return response.json();
-    })
-    .then(responseData => {
-        console.log('Salesforce response:', responseData);
-    })
-    .catch(error => {
-        console.error('Error submitting data to Salesforce:', error);
-    });
+          }
+          return response.json();
+        })
+        .then(responseData => {
+          console.log('Vine CRM response:', responseData);
+        })
+        .catch(error => {
+          console.error('Error submitting data to Vine CRM:', error);
+        });
       };
     `,
         }}
