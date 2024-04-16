740c286374130e21f388aaf0e4a0476a924d4696
Ravi Dubey <ravindrawebdeveloper2018@gmail.com>
2024-04-16 19:57:51 +0530

    chat-phonedgv

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -255,7 +255,7 @@ const MyApp = ({ Component, pageProps }: any) => {
           const labelMapping = {
               "Name": "name",
               "Email": "email",
-              "Mobile Number": "Phone",
+              "Mobile Number": "mobile",
               "Programme of Interest": "interest"
           };
           const propertyName = labelMapping[item.label] || item.label; 
