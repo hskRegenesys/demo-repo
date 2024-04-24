baad2433516af1d1a3c86fffac8432843e53aa3b
mohit-lehri <mohitl@regenesys.net>
2024-04-24 14:55:01 +0530

    make static page homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import dynamic from "next/dynamic";
 
 // import HomeBanner from "@/components/HomeBanner/HomeBanner";
 // import HomeCourses from "@/components/HomeCourses/HomeCourses";
-// import HeaderOne from "@/components/Header/HeaderOne";
+import HeaderOne from "@/components/Header/HeaderOne";
 // import MobileMenu from "@/components/Header/MobileMenu";
 // import CallToSection from "@/components/HomeSkillDescription/CallToSection";
 // import HomeSkillDescription from "@/components/HomeSkillDescription/HomeSkillDescription";
@@ -48,43 +48,43 @@ const CallToSection = dynamic(
   () => import("@/components/HomeSkillDescription/CallToSection")
 );
 const MobileMenu = dynamic(() => import("@/components/Header/MobileMenu"));
-const HeaderOne = dynamic(() => import("@/components/Header/HeaderOne"));
 const HomeBanner = dynamic(() => import("@/components/HomeBanner/HomeBanner"));
 const HomeCourses = dynamic(
   () => import("@/components/HomeCourses/HomeCourses")
 );
-
 import _ from "lodash";
 import Schemas from "../schemas";
 import { Constants } from "src/schemas/data";
 import StickyBar from "@/components/StickyFooter/Sticky";
-
-import { allCourseList } from "@/data/courseData";
-
+// import { allCourseList } from "@/data/courseData";
 import { Modal } from "react-bootstrap";
 import ImageModalPopup from "@/components/Modal/ImageModalPopup";
 import ThankYouPopup from "@/components/Modal/ThankYouPopup";
 import GoogleMap from "@/components/GoogleMap/GoogleMap";
 import TestimonialsVideo from "@/components/TestimonialsVideo/testimonialsVideo";
-import videoTestimonialData from "@/data/videoTestimonial";
+// import videoTestimonialData from "@/data/videoTestimonial";
 import PopupForm from "@/components/NewComponents/popupForm/PopupForm";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import PopupData from "@/components/NewComponents/popupForm/PopupData";
 
-const Home2 = () => {
+const Home2 = ({ initialCourses, initialVideoTestimonialData }: any) => {
   const [show, setShow] = useState(false);
   const [thankYouShow, setThankYouShow] = useState<boolean>(false);
   const [isPopupVisible, setIsPopupVisible] = useState(false);
+  const [courses, setCourses] = useState(initialCourses);
+  const [videoTestimonialData, setVideoTestimonialData] = useState(
+    initialVideoTestimonialData
+  );
 
-  let courses: any = [];
+  // let courses: any = [];
 
-  if (allCourseList?.length) {
-    courses = _.filter(
-      allCourseList,
-      (item: any) => item?.isAddon === false && item?.mode_id === 1
-    );
-  }
+  // if (allCourseList?.length) {
+  //   courses = _.filter(
+  //     allCourseList,
+  //     (item: any) => item?.isAddon === false && item?.mode_id === 1
+  //   );
+  // }
 
   useEffect(() => {
     const timeoutModal = setTimeout(() => {
@@ -159,5 +159,36 @@ const Home2 = () => {
     </Layout>
   );
 };
+
+export async function getStaticProps() {
+  try {
+    const { allCourseList } = await import("@/data/courseData");
+    const { default: videoTestimonialData } = await import(
+      "@/data/videoTestimonial"
+    );
+
+    let initialCourses: any = [];
+
+    if (allCourseList?.length) {
+      initialCourses = _.filter(
+        allCourseList,
+        (item: any) => item?.isAddon === false && item?.mode_id === 1
+      );
+    }
+    return {
+      props: {
+        initialCourses,
+        initialVideoTestimonialData: videoTestimonialData,
+      },
+    };
+  } catch (error) {
+    return {
+      props: {
+        initialCourses: [],
+        initialVideoTestimonialData: [],
+      },
+    };
+  }
+}
 
 export default Home2;
