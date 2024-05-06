fdc032b6cc6af3cc3c0c119813ccefa05e7936d3
GunasekarRajeshkumar <gunasekarr@regenesys.net>
2024-05-06 13:18:30 +0530

    Home-banner-changes

--- src/data/newComponentData/commonComponentData/homeSliderBannerData.tsx
+++ src/data/newComponentData/commonComponentData/homeSliderBannerData.tsx
@@ -1,30 +1,36 @@
 const homeSliderBannerData = {
   sliderDataDesktop: [
     {
-      imageUrl: "/assets/images/new-component-assets/Hero-Banner.webp",
+      imageUrl:
+        "/assets/images/new-component-assets/Home-page/Home-Banner-desktop-slider-one.webp",
       title: "Slide 1",
     },
     {
-      imageUrl: "/assets/images/new-component-assets/Hero-Banner.webp",
+      imageUrl:
+        "/assets/images/new-component-assets/Home-page/Home-Banner-desktop-slider-one.webp",
       title: "Slide 2",
     },
     {
-      imageUrl: "/assets/images/new-component-assets/Hero-Banner.webp",
+      imageUrl:
+        "/assets/images/new-component-assets/Home-page/Home-Banner-desktop-slider-one.webp",
       title: "Slide 3",
     },
     // Add more slides as needed
   ],
   sliderDataMobile: [
     {
-      imageUrl: "/assets/images/new-component-assets/Hero Banner - Mobile.webp",
+      imageUrl:
+        "/assets/images/new-component-assets/Home-page/Home-Banner-mobile-slider-one.webp",
       title: "Slide 1",
     },
     {
-      imageUrl: "/assets/images/new-component-assets/Hero Banner - Mobile.webp",
+      imageUrl:
+        "/assets/images/new-component-assets/Home-page/Home-Banner-mobile-slider-one.webp",
       title: "Slide 2",
     },
     {
-      imageUrl: "/assets/images/new-component-assets/Hero Banner - Mobile.webp",
+      imageUrl:
+        "/assets/images/new-component-assets/Home-page/Home-Banner-mobile-slider-one.webp",
       title: "Slide 3",
     },
     // Add more slides as needed
