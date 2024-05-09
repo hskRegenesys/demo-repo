ff22b68510fa55b8b759dd5df7c9616eac31780e
Ravi Dubey <ravindrawebdeveloper2018@gmail.com>
2024-05-09 19:00:29 +0530

    pricing currency update

--- src/data/refundPolicy.tsx
+++ src/data/refundPolicy.tsx
@@ -6,26 +6,26 @@ export const refundPolicy = {
 
   titleAdmissionFee: "Admin Fee",
   titleAdmissionFeeParaph:
-    "Upon enrolment in any program at Digital Regenesys, students are required to pay a non-refundable administrative fee of R 5,00, N 25,000, KSH 2,500, UGX 65,000, TZN 50,000 (included in the price of the programme). This fee covers administrative costs and is applicable regardless of program completion.",
+    "Upon enrolment in any program at Digital Regenesys, students are required to pay a non-refundable administrative fee of R 5,00, ₦ 25,000, KSh 2,500, UGX 65,000, TSh 50,000 (included in the price of the programme). This fee covers administrative costs and is applicable regardless of program completion.",
 
   titleRefundStructure: "Refund Structure",
 
   list1:
-    "Cancellation Before Program Start Date: If a student cancels before the program's start date, they will receive a refund of 100% of the program fee, minus the admin fee. (R 500, N 25,000,KSH 2,500,UGX 65,000,TZN 50,000)  ",
+    "Cancellation Before Program Start Date: If a student cancels before the program's start date, they will receive a refund of 100% of the program fee, minus the admin fee. (R 500, N 25,000, KSH 2,500, UGX 65,000, TZN 50,000)  ",
   list2:
     "Start Date or Within the First Week: For withdrawals on the program's start date or within the first week, students will be eligible for a refund of R 1,000, N 50,000, KSH 5,000, UGX 130,000, TZN 100,000 of the remaining program fee, after deducting the admin fee.",
   details: [
     {
-      item: "Cancellation Before Program Start Date: If a student cancels before the program's start date, they will receive a refund of 100% of the program fee, minus the admin fee. (R 500, N 25,000, KSH 2,500, UGX 65,000, TZN 50,000)",
+      item: "Cancellation Before Program Start Date: If a student cancels before the program's start date, they will receive a refund of 100% of the program fee, minus the admin fee. (R 500, ₦  25,000, KSh 2,500, UGX 65,000, TSh 50,000)",
     },
     {
-      item: "Start Date or Within the First Week: For withdrawals on the program's start date or within the first week, students will be eligible for a refund of R 1,000, N 50,000, KSH 5,000, UGX 130,000, TZN 100,000 of the remaining program fee, after deducting the admin fee.",
+      item: "Start Date or Within the First Week: For withdrawals on the program's start date or within the first week, students will be eligible for a refund of R 1,000, ₦ 50,000, KSh 5,000, UGX 130,000, TSh 100,000 of the remaining program fee, after deducting the admin fee",
     },
     {
-      item: "Week 2: Withdrawals between the end of the first week and the end of the second week will result in a refund of balance paid amount after deducting  R 1500, N 75,000, KSG 7,500, UGX 195,000, TZN 150,000      ",
+      item: "Week 2: Withdrawals between the end of the first week and the end of the second week will result in a refund of balance paid amount after deducting  R 1500, ₦ 75,000, KSh 7,500, UGX 195,000, TSh 150,000",
     },
     {
-      item: "Week 3: Withdrawals between the end of the second week and the end of the third week will result in a refund of balance paid amount after deducting of R 2,000, N 100,000, KSH 10,000, UGX 260,000, TZN 200,000",
+      item: "Week 3: Withdrawals between the end of the second week and the end of the third week will result in a refund of balance paid amount after deducting of R 2,000, ₦ 100,000, KSh 10,000, UGX 260,000, TSh 200,000",
     },
   ],
 
