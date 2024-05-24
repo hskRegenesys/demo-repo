4eb330543fa483468bf58e17bf5790dd7fc828d7
GunasekarRajeshkumar <gunasekarr@regenesys.net>
2024-05-24 20:52:45 +0530

    bug-fixed

--- src/data/newComponentData/commonComponentData/homeSliderBannerData.tsx
+++ src/data/newComponentData/commonComponentData/homeSliderBannerData.tsx
@@ -1,13 +1,16 @@
 const homeSliderBannerData = {
   sliderDataDesktop: [
     {
-      imageUrl: "Images/Banner/Home-Banner-Desktop-slider-one.png",
-      title: "Slide 1",
-    },
-    {
       imageUrl: "Images/Banner/Home-Banner-Desktop-slider-two.png",
       title: "Slide 2",
+      link: "https://uat-demo.digitalregenesys.com/all-courses",
     },
+    {
+      imageUrl: "Images/Banner/Home-Banner-Desktop-slider-one.png",
+      title: "Slide 1",
+      link: "https://uat-demo.digitalregenesys.com/all-courses/artificial-intelligence",
+    },
+
     // {
     //   imageUrl: "Images/Banner/Home-Banner-Desktop-slider-three.png",
     //   title: "Slide 3",
@@ -15,13 +18,16 @@ const homeSliderBannerData = {
   ],
   sliderDataMobile: [
     {
-      imageUrl: "Images/Banner/Home-Banner-Mobile-slider-one.png",
-      title: "Slide 1",
-    },
-    {
       imageUrl: "Images/Banner/Home-Banner-Mobile-slider-two.png",
       title: "Slide 2",
+      link: "https://uat-demo.digitalregenesys.com/all-courses",
     },
+    {
+      imageUrl: "Images/Banner/Home-Banner-Mobile-slider-one.png",
+      title: "Slide 1",
+      link: "https://uat-demo.digitalregenesys.com/all-courses/artificial-intelligence",
+    },
+
     // {
     //   imageUrl: "Images/Banner/Home-Banner-Mobile-slider-three.png",
     //   title: "Slide 3",
