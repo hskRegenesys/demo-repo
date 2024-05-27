5784512b5ad1789657dddc1d11700873ed516e0c
Ravi Dubey <ravindrawebdeveloper2018@gmail.com>
2024-05-27 19:31:06 +0530

    career transform module

--- src/components/NewComponents/careersTransformed/CareersTransformed.tsx
+++ src/components/NewComponents/careersTransformed/CareersTransformed.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Styles from "./careersTransformed.module.css";
-import SwiperCore, { Navigation, Pagination, Autoplay } from "swiper";
+//import SwiperCore, { Navigation, Pagination, Autoplay } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/swiper-bundle.css";
 import careersTransformedData from "../../../data/newComponentData/commonComponentData/careersTransformedData";
 import Image from "next/image";
+import MultiCarousel from "@/components/multiCarousel/multiCarousel";
 
-SwiperCore.use([Navigation, Pagination, Autoplay]);
+// SwiperCore.use([Navigation, Pagination, Autoplay]);
 
 interface FourCardData {
   card1: { percentage: string; text: string };
@@ -52,13 +53,39 @@ const CareersTransformed: React.FC<CareersTransformedProps> = ({
     swiperCardContainer,
   } = mainCourseData;
 
-  const swiperSettings = {
-    pagination: { clickable: true },
-    spaceBetween: 25,
-    autoplay: {
-      delay: 5000,
-      speed: 5000,
-    },
+  const settings = {
+    dots: true,
+    //infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    //autoplay: true,
+    //speed: 2000,
+    //autoplaySpeed: 2000,
+
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          initialSlide: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
 
   return (
@@ -77,108 +104,40 @@ const CareersTransformed: React.FC<CareersTransformedProps> = ({
         <h2 className={Styles.secondHeading}>{secondHeading}</h2>
       </div>
 
-      {/* <div className={Styles.cardsContainer}>
-        {[card1, card2, card3, card4].map((card, index) => (
-          <div key={index} className={Styles.card}>
-            <div className={Styles.cardTop}>
-              {"percentage" in card && (
-                <p className={Styles.cardtext1}>{card.percentage}</p>
-              )}
-              {"rating" in card && (
-                <>
-                  <p className={Styles.cardtext1}>{card.rating}</p>
-                  {card.ratingImg && (
-                    <img
-                      className={Styles.ratingImg}
-                      src={card.ratingImg}
-                      alt="Rating"
-                    />
-                  )}
-                </>
-              )}
-            </div>
-            <div className={Styles.cardBottom}>
-              <p className={Styles.cardText}>{card.text}</p>
-            </div>
-          </div>
-        ))}
-      </div> */}
-
-      <div className={Styles.swiperContainer}>
-        <Swiper className={Styles.swiperStyle} {...swiperSettings}>
-          {swiperCardContainer.map((swiperCard, index) => (
-            <SwiperSlide key={index} className={Styles.swiperCard}>
-              <div className={Styles.topSection}>
-                <div className={Styles.profile}>
-                  <div className={Styles.profileImg}>
-                    <Image
-                      src={`${imageUrl}${swiperCard.profileImg}`}
-                      alt={swiperCard.name}
-                      title={swiperCard.name}
-                      width={80}
-                      height={80}
-                    />
-                  </div>
-                  <div className={Styles.nameRole}>
-                    <h4 className={Styles.name}>{swiperCard.name}</h4>
-                    <p className={Styles.role}>{swiperCard.role}</p>
+      <div className={Styles.CTStyle}>
+        <div className="container text-center">
+          <MultiCarousel
+            className={Styles.swiperStyle}
+            childSettings={settings}
+          >
+            {swiperCardContainer.map((swiperCard, index) => (
+              <div key={index} className={Styles.swiperCard}>
+                <div className={Styles.topSection}>
+                  <div className={Styles.profile}>
+                    <div className={Styles.profileImg}>
+                      <Image
+                        src={`${imageUrl}${swiperCard.profileImg}`}
+                        alt={swiperCard.name}
+                        title={swiperCard.name}
+                        width={80}
+                        height={80}
+                      />
+                    </div>
+                    <div className={Styles.nameRole}>
+                      <h4 className={Styles.name}>{swiperCard.name}</h4>
+                      <p className={Styles.role}>{swiperCard.role}</p>
+                    </div>
                   </div>
                 </div>
-                {/* <div className={Styles.smallCard}>
-                  <div className={Styles.smallCardIndide}>
-                    <img
-                      className={Styles.riseImg}
-                      src={swiperCard.riseImg}
-                      alt="Rise"
-                    />
-                    <h2>50%</h2>
-                  </div>
-                  <p className={Styles.increment}>Increment in salary</p>
+                <div className={Styles.contentSection}>
+                  <p className={Styles.experienceText}>
+                    {swiperCard.experienceText}
+                  </p>
                 </div>
-                <div
-                  className={Styles.playVideo}
-                  onClick={() =>
-                    handleEnrollButtonVidio(swiperCard.StudentVidio)
-                  }
-                >
-                  <div className={Styles.Vidio}>
-                    <img src={swiperCard.playImg} alt="Rise" />
-                  </div>
-
-                  <span>Hear My Story</span>
-                </div> */}
               </div>
-              <div className={Styles.contentSection}>
-                <p className={Styles.experienceText}>
-                  {swiperCard.experienceText}
-                </p>
-              </div>
-              {/* <div className={Styles.downSection}>
-                <div className={Styles.roleCard1}>
-                  <p className={Styles.roleText}>{swiperCard.firstRole}</p>
-                  <img
-                    className={Styles.companyImg}
-                    src={swiperCard.companyFirstRoleImg}
-                    alt="Company"
-                  />
-                </div>
-                <img
-                  className={Styles.arrowImg}
-                  src="/assets/images/new-component-assets/Arrow.webp"
-                  alt="Arrow"
-                />
-                <div className={Styles.roleCard2}>
-                  <p className={Styles.roleText}>{swiperCard.secondRole}</p>
-                  <img
-                    className={Styles.companyImg}
-                    src={swiperCard.companySecondRoleImg}
-                    alt="Company"
-                  />
-                </div>
-              </div> */}
-            </SwiperSlide>
-          ))}
-        </Swiper>
+            ))}
+          </MultiCarousel>
+        </div>
       </div>
 
       <div className={Styles.ctFrame}>
