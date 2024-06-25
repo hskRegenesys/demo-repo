dd387a403e054086ce9ed041c18f056282859ef2
mohit-lehri <mohitl@regenesys.net>
2024-06-25 20:36:29 +0530

    new whatsapp changes-2

--- src/pages/api/sendWhatsAppMessage.js
+++ src/pages/api/sendWhatsAppMessage.js
@@ -1,10 +1,5 @@
 const { WhatsAppMessagingAPIConstants } = require("../../data/axisos");
 
-const productToken =
-  process.env.CM_PRODUCT_TOKEN || "5a2cc47d-2f63-4fe9-85ef-26fcede14984";
-const namespaceToken =
-  process.env.CM_NAMESPACE || "1b8c1d6e_a041_4986_9875_d1c247485578";
-
 export default async function sendWhatsAppMessage(req, res) {
   if (req.method === "POST") {
     const { Name, Phone } = req?.body?.formData;
@@ -13,7 +8,7 @@ export default async function sendWhatsAppMessage(req, res) {
     const messageData = {
       messages: {
         authentication: {
-          producttoken: productToken,
+          producttoken: process.env.CM_PRODUCT_TOKEN,
         },
         msg: [
           {
@@ -33,7 +28,7 @@ export default async function sendWhatsAppMessage(req, res) {
                 {
                   template: {
                     whatsapp: {
-                      namespace: namespaceToken,
+                      namespace: process.env.CM_NAMESPACE,
 
                       element_name: "wa_openday_zoom_link",
                       language: {
