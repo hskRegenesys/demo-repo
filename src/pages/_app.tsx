2ac5c4f5b537be41a5f07f05deb1ed1602101e00
GunasekarRajeshkumar <gunasekarr@regenesys.net>
2024-07-03 15:37:26 +0530

    partytown-added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -43,7 +43,6 @@ const MyApp = ({ Component, pageProps }: any) => {
       <Schemas type={Constants.localbusiness} />
       {/* Pixel code script start */}
       <Toaster />
-
       <Script
         id="facebook_script"
         strategy="beforeInteractive"
@@ -62,40 +61,6 @@ const MyApp = ({ Component, pageProps }: any) => {
               `,
         }}
       />
-
-      {/* Pixel code script end */}
-
-      {/* GTM code start */}
-      <Script
-        id="google-tag-manager"
-        strategy="lazyOnload"
-        dangerouslySetInnerHTML={{
-          __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-M9XVQW4');
-              `,
-        }}
-      />
-      {/* GTM code end */}
-
-      {/* Google Tag Manager GTM code nanddeep start */}
-      <Script
-        id="google-tag-manager"
-        dangerouslySetInnerHTML={{
-          __html: `
-          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-K4F2983');
-              `,
-        }}
-      />
-      {/* GTM code end */}
-
       {/* linked start */}
       <Script
         id="snap_licdn"
@@ -115,47 +80,6 @@ const MyApp = ({ Component, pageProps }: any) => {
           s.parentNode.insertBefore(b, s);})(window.lintrk);
          
           `,
-        }}
-      />
-      {/* linked end */}
-
-      {/* Google Tag Script Start  */}
-      <Script
-        id="google-tag-manager"
-        async
-        src="https://www.googletagmanager.com/gtag/js?id=AW-873313553"
-      />
-      {/* <!-- Google tag (gtag.js) --> Ramesh  */}
-      <Script
-        id="google-tag-manager"
-        async
-        src="https://www.googletagmanager.com/gtag/js?id=UA-232125756-1"
-      />
-
-      <Script
-        id="google-tag-manager"
-        strategy="beforeInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'UA-232125756-1');
-              `,
-        }}
-      />
-      {/* <!-- Google tag (gtag.js) --> Ramesh END */}
-      <Script
-        id="google-tag-manager"
-        strategy="beforeInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'AW-873313553');
-            gtag('config', 'AW-10930930903');
-              `,
         }}
       />
       {/* linked start */}
@@ -180,16 +104,6 @@ const MyApp = ({ Component, pageProps }: any) => {
         }}
       />
 
-      {/* linked end */}
-      {/* <noscript>
-        <iframe
-          src="https://www.facebook.com/tr?id=613242390822464&ev=PageView&noscript=1"
-          height="0"
-          width="0"
-          style={{ display: "none", visibility: "hidden" }}
-        ></iframe>
-      </noscript> */}
-
       <noscript>
         <img
           height="1"
@@ -209,19 +123,6 @@ const MyApp = ({ Component, pageProps }: any) => {
           src="https://px.ads.linkedin.com/collect/?pid=5438577&fmt=gif"
         />
       </noscript>
-
-      {/* Google Tag Manager (noscript) */}
-
-      <noscript>
-        <iframe
-          src="https://www.googletagmanager.com/ns.html?id=GTM-K4F2983"
-          height="0"
-          width="0"
-          style={{ display: "none", visibility: "hidden" }}
-        ></iframe>
-      </noscript>
-
-      {/*End Google Tag Manager (noscript) */}
 
       {/* <Script
         type="text/javascript"
