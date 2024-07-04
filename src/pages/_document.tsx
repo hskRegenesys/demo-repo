9a639abd33d42a6a6f02d30c0f43de4d4386eb79
GunasekarRajeshkumar <159122506+GunasekarRajeshkumar@users.noreply.github.com>
2024-07-04 13:49:18 +0530

    Revert "partytown-added"

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,109 +1,12 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import Script from "next/script";
-import { Partytown } from "@builder.io/partytown/react";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <Partytown debug={true} logScriptExecution={true} />
-        {/* GTM code start */}
-        <Script
-          type="text/partytown"
-          id="google-tag-manager"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-M9XVQW4');
-              `,
-          }}
-        />
-        {/* GTM code end */}
-
-        {/* Google Tag Manager GTM code nanddeep start */}
-        <Script
-          type="text/partytown"
-          id="google-tag-manager"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-K4F2983');
-              `,
-          }}
-        />
-        {/* GTM code end */}
-
-        {/* Google Tag Script Start  */}
-        <Script
-          id="google-tag-manager"
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=AW-873313553"
-        />
-        {/* <!-- Google tag (gtag.js) --> Ramesh  */}
-        <Script
-          id="google-tag-manager"
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=UA-232125756-1"
-        />
-
-        <Script
-          id="google-tag-manager"
-          strategy="beforeInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'UA-232125756-1');
-              `,
-          }}
-        />
-        {/* <!-- Google tag (gtag.js) --> Ramesh END */}
-        <Script
-          id="google-tag-manager"
-          strategy="beforeInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'AW-873313553');
-            gtag('config', 'AW-10930930903');
-              `,
-          }}
-        />
-      </Head>
+      <Head></Head>
       <body>
         <Main />
         <NextScript />
-        {/* Google Tag Manager (noscript) */}
-
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-K4F2983"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          ></iframe>
-        </noscript>
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-M9XVQW4"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          ></iframe>
-        </noscript>
-
-        {/*End Google Tag Manager (noscript) */}
       </body>
     </Html>
   );
